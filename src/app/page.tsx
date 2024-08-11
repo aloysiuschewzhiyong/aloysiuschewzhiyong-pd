bcbb0ecab0fe7fb474fda5be0173eebd63098edb
chickennuggetcurrysauce <22001095@myrp.edu.sg>
2024-08-11 23:40:49 +0800

    adjusted tilt sens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,16 @@ import { HoverBorderGradient } from "@/components/ui/hover-border-gradient";
 import { BorderBeam } from "@/components/ui/border-beam";
 import ScrollDrivenScribbles from "@/components/scroll-scribble";
 import Image from "next/image";
-import Tilt from 'react-parallax-tilt';
+import Tilt from "react-parallax-tilt";
 
 // Utility function to detect device type
 function getDeviceType() {
   const ua = navigator.userAgent;
-  if (/Mobile|Android|iP(hone|od)|IEMobile|WPDesktop|Opera Mini|BlackBerry/.test(ua)) {
+  if (
+    /Mobile|Android|iP(hone|od)|IEMobile|WPDesktop|Opera Mini|BlackBerry/.test(
+      ua
+    )
+  ) {
     return "Mobile";
   } else if (/Tablet|iPad/.test(ua)) {
     return "Tablet";
@@ -71,31 +75,30 @@ export default function Home() {
       <FadeInWhenVisible>
         <section className="min-h-[100vh]">
           <div className="relative max-w-6xl mx-auto p-6 -mt-24 z-10">
-          <Tilt tiltMaxAngleX={10} tiltMaxAngleY={10}>
-  <div className="w-full rounded-2xl shadow-xl dark:shadow-reactive border bg-white/[0.69] dark:bg-black/[0.19] border-black/[0.1] dark:border-white/[0.2] backdrop-blur-md h-auto">
-    {/* Conditionally render BorderBeam only if device type is not Mobile */}
-    {deviceType !== "Mobile" && <BorderBeam />}
-    <div className="flex justify-start items-center space-x-1.5 p-4">
-      <span className="w-3 h-3 rounded-full bg-red-400 hover:bg-red-700"></span>
-      <span className="w-3 h-3 rounded-full bg-yellow-400 hover:bg-yellow-500"></span>
-      <span className="w-3 h-3 rounded-full bg-green-400 hover:bg-green-600"></span>
-    </div>
+            <Tilt tiltMaxAngleX={5} tiltMaxAngleY={5}>
+              <div className="w-full rounded-2xl shadow-xl dark:shadow-reactive border bg-white/[0.69] dark:bg-black/[0.19] border-black/[0.1] dark:border-white/[0.2] backdrop-blur-md h-auto">
+                {/* Conditionally render BorderBeam only if device type is not Mobile */}
+                {deviceType !== "Mobile" && <BorderBeam />}
+                <div className="flex justify-start items-center space-x-1.5 p-4">
+                  <span className="w-3 h-3 rounded-full bg-red-400 hover:bg-red-700"></span>
+                  <span className="w-3 h-3 rounded-full bg-yellow-400 hover:bg-yellow-500"></span>
+                  <span className="w-3 h-3 rounded-full bg-green-400 hover:bg-green-600"></span>
+                </div>
 
-    <div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-9 gap-0">
-        <div className="m-10 col-span-1 sm:col-span-1 md:col-span-4 text-white rounded">
-          <ScrollDrivenScribbles imagePath="/IMG_1035.JPG" />
-        </div>
-        <div className="m-10 my-10 col-span-1 sm:col-span-1 md:col-span-5 text-white rounded">
-          <span className="bg-gradient-to-b from-gray-500 to-black bg-clip-text text-transparent dark:from-white dark:to-gray-400 font-bold leading-none text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
-            About Me
-          </span>
-        </div>
-      </div>
-    </div>
-  </div>
-</Tilt>
-
+                <div>
+                  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-9 gap-0">
+                    <div className="m-10 col-span-1 sm:col-span-1 md:col-span-4 text-white rounded">
+                      <ScrollDrivenScribbles imagePath="/IMG_1035.JPG" />
+                    </div>
+                    <div className="m-10 my-10 col-span-1 sm:col-span-1 md:col-span-5 text-white rounded">
+                      <span className="bg-gradient-to-b from-gray-500 to-black bg-clip-text text-transparent dark:from-white dark:to-gray-400 font-bold leading-none text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
+                        About Me
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Tilt>
           </div>
         </section>
       </FadeInWhenVisible>
