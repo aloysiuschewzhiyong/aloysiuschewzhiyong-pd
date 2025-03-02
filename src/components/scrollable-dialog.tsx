bf5f94ed9edbd54a79b7b3566dd432f00e96ed79
Aloysius Chew <aloysiuschewzhiyong@gmail.com>
2025-03-02 21:16:22 +0800

    fix build errors

--- src/components/scrollable-dialog.tsx
+++ src/components/scrollable-dialog.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { Button } from "@/components/ui/button"
+import * as React from "react";
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
@@ -9,11 +9,11 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-import { ScrollArea } from "@/components/ui/scroll-area"
+} from "@/components/ui/dialog";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export function ScrollableDialog() {
-  const [open, setOpen] = React.useState(false)
+  const [open, setOpen] = React.useState(false);
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -23,21 +23,29 @@ export function ScrollableDialog() {
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>Scrollable Content</DialogTitle>
-          <DialogDescription>This dialog contains a scrollable area for long content.</DialogDescription>
+          <DialogDescription>
+            This dialog contains a scrollable area for long content.
+          </DialogDescription>
         </DialogHeader>
         <ScrollArea className="h-[300px] w-full rounded-md border p-4">
           <div className="space-y-4">
-            <p>This is a paragraph of text that demonstrates scrolling within the dialog.</p>
-            <p>You can add as much content as you need here, and it will be scrollable.</p>
+            <p>
+              This is a paragraph of text that demonstrates scrolling within the
+              dialog.
+            </p>
+            <p>
+              You can add as much content as you need here, and it will be
+              scrollable.
+            </p>
             {Array.from({ length: 15 }).map((_, index) => (
               <p key={index}>
-                This is paragraph {index + 1}. It's here to create enough content to make the dialog scrollable.
+                This is paragraph {index + 1}. It&apos;s here to create enough
+                content to make the dialog scrollable.
               </p>
             ))}
           </div>
         </ScrollArea>
       </DialogContent>
     </Dialog>
-  )
+  );
 }
-
